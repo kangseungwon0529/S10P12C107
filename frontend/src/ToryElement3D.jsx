4f1eaafe24507aa1c3e8f95c6e7e22d7c66e46cd
강승원 <isjj62@naver.com>
2024-02-02 18:00:23 +0900

    마지막 잎새

--- frontend/src/ToryElement3D.jsx
+++ frontend/src/ToryElement3D.jsx
@@ -2,6 +2,7 @@ import { OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { Math } from "three.js";
+import {THREE}
 
 function ToryElement3D(){
 
@@ -12,7 +13,7 @@ function ToryElement3D(){
     return(
         <>
         {/* 조명 */}
-        <directionalLight position={[1,1,1]} />
+        <directionalLight position={[1,0,0]} />
         <axesHelper scale={10}/>
 
         {/* 화면상 요녀석 적용하고 쓰려면 npm install @react-three/drei*/}
@@ -22,7 +23,8 @@ function ToryElement3D(){
         // y축으로 2만큼 이동해라 position={[0,2,0]}랑 같은 기능을 하는것
         position-y={2}
         // 회전하는 상자 만들건데 라디안 적용해줘서 넣어라rotation={{0,45*Math.PI/180,0}}
-        rotation-y={45*Math.PI/180}
+        rotation-y={THREE.MathUtils.degToRad(45)}
+        scale={[2,1,1]}
         >
             {/* 박스 재질 */}
             <boxGeometry/>
